56ee748e7021a3179fbe322cd8a1eca4dc270b48
Josemascherpa <joseimascherpa@gmail.com>
2024-02-29 22:56:17 -0300

    Eliminated classes

--- app/src/main/java/com/example/webscraping/Loading.java
+++ app/src/main/java/com/example/webscraping/Loading.java
@@ -28,7 +28,7 @@ public class Loading extends AppCompatActivity {
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.setStatusBarColor(Loading.this.getResources().getColor(R.color.black));
+        window.setStatusBarColor(0xFF000000);
 
         LoadDataRio();
         versionTV = (TextView)findViewById(R.id.version);
@@ -37,51 +37,39 @@ public class Loading extends AppCompatActivity {
     }
 
     public void LoadDataRio(){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Document doc = Jsoup.connect(urlWeb).get();
-                    Element valor = doc.getElementsByClass("SANTA FE").first();
-                    Element row = doc.select("tr:contains(SANTA FE):not(:contains(San Javier))").first();
-                    String location = row.select("[data-label=Puerto:]").text();
-                    String rio = row.select("[data-label=Río:]").text(); // Paraná
-                    String altura = row.select("[data-label=Ultimo Registro:]").text(); // 4,00
-                    String variacion = row.select("[data-label=Variacion]").text(); // -0,05
-                    String periodo = row.select("[data-label=Periodo:]").text();
-                    String fecha = row.select("[data-label=Fecha Hora:]").text();
-                    String estado = row.select("[data-label=Estado:]").text();
-                    String variacionAnterior = row.select("[data-label=Registro Anterior:]").text();
-                    String fechaAnterior = row.select("[data-label=Fecha Anterior:]").text();
+        new Thread(() -> {
+            try {
+                Document doc = Jsoup.connect(urlWeb).get();
+                Element row = doc.select("tr:contains(SANTA FE):not(:contains(San Javier))").first();
+                String altura = row.select("[data-label=Ultimo Registro:]").text(); // 4,00
+                String variacion = row.select("[data-label=Variacion]").text(); // -0,05
+                String periodo = row.select("[data-label=Periodo:]").text();
+                String fecha = row.select("[data-label=Fecha Hora:]").text();
+                String estado = row.select("[data-label=Estado:]").text();
+                String variacionAnterior = row.select("[data-label=Registro Anterior:]").text();
+                String fechaAnterior = row.select("[data-label=Fecha Anterior:]").text();
 
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
+                runOnUiThread(() -> {
 
-                            Class MainActivity = com.example.webscraping.MainActivity.class;
-                            Intent intent = new Intent(Loading.this,MainActivity);
-                            Bundle bundle = new Bundle();
-                            bundle.putString("location", location);
-                            bundle.putString("rio", rio);
-                            bundle.putString("altura", altura);
-                            bundle.putString("variacion", variacion);
-                            bundle.putString("periodo", periodo);
-                            bundle.putString("fecha", fecha);
-                            bundle.putString("estado", estado);
-                            bundle.putString("variacionAnterior", variacionAnterior);
-                            bundle.putString("fechaAnterior", fechaAnterior);
-                            intent.putExtras(bundle);
+                    Class MainActivity = com.example.webscraping.MainActivity.class;
+                    Intent intent = new Intent(Loading.this,MainActivity);
+                    Bundle bundle = new Bundle();
+                    bundle.putString("altura", altura);
+                    bundle.putString("variacion", variacion);
+                    bundle.putString("periodo", periodo);
+                    bundle.putString("fecha", fecha);
+                    bundle.putString("estado", estado);
+                    bundle.putString("variacionAnterior", variacionAnterior);
+                    bundle.putString("fechaAnterior", fechaAnterior);
+                    intent.putExtras(bundle);
 
-                            startActivity(intent);
-                            finish();
-                        }
-                    });
+                    startActivity(intent);
+                    finish();
+                });
 
-                } catch (IOException e) {
-                    Log.i("Error", e.getMessage() + " "+"ERRORRR");
-                }
+            } catch (IOException e) {
+                Log.i("Error", e.getMessage() + " "+"ERRORRR");
             }
-
         }).start();
     }
 
