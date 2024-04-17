49f3ea25af9f27c84800b0890d2fdee597a64f47
Josemascherpa <joseimascherpa@gmail.com>
2024-04-16 22:40:39 -0300

    New UI Login

---
+++ app/src/main/java/com/example/webscraping/UI/ManagerUILoading.java
@@ -0,0 +1,115 @@
+package com.example.webscraping.UI;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+
+import com.example.webscraping.databinding.LoadinguiBinding;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+public class ManagerUILoading {
+    private LoadinguiBinding binding;
+    FrameLayout bottomSheet;
+    BottomSheetBehavior<View> bottomSheetBehavior;
+
+    FrameLayout bottomSheetLogin;
+    BottomSheetBehavior<View> bottomSheetBehaviorLogin;
+
+    public ManagerUILoading(LoadinguiBinding bindingLoading){
+        this.binding = bindingLoading;
+        SetsBottomSheets();
+
+    }
+
+    public void ClickButtonRegister(Button btnRegister){
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                SingUp();
+            }
+        });
+    }
+
+    public void ClickButtonLogin(Button btnLogin){
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Login();
+            }
+        });
+    }
+
+    private void Login(){
+        bottomSheetLogin.setVisibility(View.VISIBLE);
+        bottomSheet.setVisibility(View.INVISIBLE);
+        bottomSheetBehaviorLogin.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+    }
+
+    private void SingUp(){
+        bottomSheetLogin.setVisibility(View.INVISIBLE);
+        bottomSheet.setVisibility(View.VISIBLE);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+    }
+
+    private void SetsBottomSheets(){
+        bottomSheet = binding.standardBottomSheet;
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if(newState==BottomSheetBehavior.STATE_EXPANDED){
+                    binding.comenzaraventura.setEnabled(false);
+                    binding.ingresar.setEnabled(false);
+                }else if(newState==BottomSheetBehavior.STATE_COLLAPSED || newState == BottomSheetBehavior.STATE_HIDDEN){
+                    binding.comenzaraventura.setEnabled(true);
+                    binding.ingresar.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+
+        bottomSheetLogin = binding.loginBottomSheets;
+        bottomSheetBehaviorLogin = BottomSheetBehavior.from(bottomSheetLogin);
+
+        bottomSheetBehaviorLogin.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if(newState==BottomSheetBehavior.STATE_EXPANDED){
+                    binding.comenzaraventura.setEnabled(false);
+                    binding.ingresar.setEnabled(false);
+                }else if(newState==BottomSheetBehavior.STATE_COLLAPSED || newState == BottomSheetBehavior.STATE_HIDDEN){
+                    binding.comenzaraventura.setEnabled(true);
+                    binding.ingresar.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+    }
+
+    public int ReturnStateBottomSheetsSignUp(){
+        return bottomSheetBehavior.getState();
+    }
+    public int ReturnStateBottomSheetsLogin(){
+            return bottomSheetBehaviorLogin.getState();
+    }
+    public BottomSheetBehavior GetBottomSheetsSign(){
+        return bottomSheetBehavior;
+    }
+
+    public BottomSheetBehavior GetBottomSheetsLogin(){
+        return bottomSheetBehaviorLogin;
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/webscraping/load/Loading.java
+++ app/src/main/java/com/example/webscraping/load/Loading.java
@@ -3,19 +3,22 @@ package com.example.webscraping.load;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Rect;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.view.View;
+import android.view.MotionEvent;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.FrameLayout;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.webscraping.R;
+import com.example.webscraping.UI.ManagerUILoading;
 import com.example.webscraping.data.Rio;
+import com.example.webscraping.databinding.LoadinguiBinding;
 import com.example.webscraping.network.CheckInternet;
 import com.example.webscraping.network.DataProvider;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
@@ -31,13 +34,27 @@ public class Loading extends AppCompatActivity {
     String versionApp = "Version 1.0.0";
     CheckInternet checkInternet = new CheckInternet();
 
+    Button comenzarAventurabtn;
+
+    Button ingresarBtn;
+    private LoadinguiBinding binding;
+
+    private ManagerUILoading managerUI;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.example.webscraping.R.layout.loadingui);
+
+        binding = LoadinguiBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        managerUI = new ManagerUILoading(binding);
+
+        managerUI.ClickButtonRegister(binding.comenzaraventura);
+        managerUI.ClickButtonLogin(binding.ingresar);
 
 
-        FrameLayout bottomSheet = findViewById(R.id.standard_bottom_sheet);
-        BottomSheetBehavior<View> bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+
 //        bottomSheetBehavior.setSaveFlags(BottomSheetBehavior.SAVE_ALL);
 //        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
 
@@ -54,6 +71,41 @@ public class Loading extends AppCompatActivity {
 //            Toast.makeText(this,"no hay internet",Toast.LENGTH_LONG).show();
 //        }
 
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event){
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            if (managerUI.ReturnStateBottomSheetsSignUp()== BottomSheetBehavior.STATE_EXPANDED) {
+
+                Rect outRect = new Rect();
+                binding.standardBottomSheet.getGlobalVisibleRect(outRect);
+
+                if(!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    managerUI.GetBottomSheetsSign().setState(BottomSheetBehavior.STATE_COLLAPSED);
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getWindow().getDecorView().getWindowToken(), 0);
+                }
+            }
+
+            if (managerUI.ReturnStateBottomSheetsLogin()== BottomSheetBehavior.STATE_EXPANDED) {
+                Rect outRect = new Rect();
+                binding.loginBottomSheets.getGlobalVisibleRect(outRect);
+
+                if(!outRect.contains((int)event.getRawX(), (int)event.getRawY())){
+                    managerUI.GetBottomSheetsLogin().setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getWindow().getDecorView().getWindowToken(), 0);
+                }
+
+
+
+            }
+
+        }
+
+        return super.dispatchTouchEvent(event);
     }
 
 
@@ -95,9 +147,6 @@ public class Loading extends AppCompatActivity {
         Context newContext = newBase.createConfigurationContext(configuration);
         super.attachBaseContext(newContext);
     }
-
-
-
 
 
 }
